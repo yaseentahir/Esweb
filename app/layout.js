badec13564f4a203b069e784d70d981005bcec3f
Yaseen Tahir <yaseentahir85@gmail.com>
2023-07-18 12:35:45 +0500

    facebook plugin

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,7 @@ import LoadScreen from "@/Components/UI/Loading/Loading";
 import Footer from "@/Components/Sections/Footer/Footer";
 import NavbarMain from "@/Components/UI/NavbarMain/NavbarMain";
 import FooterMain from "@/Components/Sections/FooterMain/FooterMain";
+import FacebookMessenger from "@/Components/UI/FacebookMessenger/FacebookMessenger";
 
 const prompt = Prompt({
   subsets: ["latin"],
@@ -67,30 +68,7 @@ export default function RootLayout({ children }) {
         ) : (
           <NavbarMain onContactPressed={contactButtonPressed} />
         )}
-        <div id="fb-root"></div>
-        <div id="fb-customer-chat" className="fb-customerchat"></div>
-        <Script strategy="lazyOnload" id="my-script">
-          {`
-            var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute("page_id", "106967095106378");
-            chatbox.setAttribute("attribution", "biz_inbox");
-      
-            window.fbAsyncInit = function() {
-              FB.init({
-                xfbml            : true,
-                version          : 'v14.0'
-              });
-            };
-      
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-        `}
-        </Script>
+           <FacebookMessenger />
         {children}
         <section>
           {locpath === "/" ? (
